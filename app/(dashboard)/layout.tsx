e1eca580eb14dbfbda61d3e143e2fe6a2f023e42
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2024-06-19 16:10:39 +0200

    Create basic Journal page

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="h-screen w-screen relative">
+      <aside className="absolute top-0 left-0 h-full w-[200px] border-r border-black/10">
+        Mood
+      </aside>
+      <div className="ml-[200px]">
+        <header className="h-[60px] border-b border-black/10">header</header>
+        <div>{children}</div>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
